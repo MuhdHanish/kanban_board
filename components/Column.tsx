1391a80745512e9c7ae28397dfd8c17838c93b7f
MuhdHanish <muhammedhanish11@gmail.com>
2024-05-05 10:34:42 +0530

    created handledragstart function and passed to card component

--- components/Column.tsx
+++ components/Column.tsx
@@ -1,12 +1,16 @@
 import { Card } from "./Card";
-import { useState } from "react";
 import { AddCard } from "./AddCard";
-import { TColumnProps } from "@/types";
+import { DragEvent, useState } from "react";
+import { TCard, TColumnProps } from "@/types";
 import { DropIndicator } from "./DropIndicator";
 
 export const Column = ({ title, headingColor, cards, column, setCards }: TColumnProps) => {
   const [active, setActive] = useState(false);
   const filteredCards = cards?.filter(card => card?.column === column);
+
+  const hanldeDragStart = (event: DragEvent, card: TCard) => {
+    event.dataTransfer.setData("cardId", card?.id);
+  };
   return (
     <div className="w-56 shrink-0">
       <div className="mb-3 flex items-center justify-between">
@@ -17,7 +21,7 @@ export const Column = ({ title, headingColor, cards, column, setCards }: TColumn
         className={`h-full w-full transition-colors 
         ${active ? "bg-neutral-800/50" : "bg-neutral-800/0"}`}
       >
-        {filteredCards?.map(card => <Card key={card?.id} {...card} />)}
+        {filteredCards?.map(card => <Card key={card?.id} card={card} hanldeDragStart={hanldeDragStart} />)}
         <DropIndicator beforeId="-1" column={column} />
         <AddCard column={column} setCards={setCards}/>
       </div>
