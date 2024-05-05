27d7f4d66cb4f6c8b01ce88334cbefcd20dfd180
MuhdHanish <muhammedhanish11@gmail.com>
2024-05-05 10:59:01 +0530

    used motion.div insted of normal div in card component to animate

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,3 +1,4 @@
+import { motion } from "framer-motion";
 import { TCardProps } from "@/types";
 import { DropIndicator } from "./DropIndicator";
 
@@ -6,12 +7,14 @@ export const Card = ({ card, hanldeDragStart }: TCardProps) => {
   return (
     <>
       <DropIndicator beforeId={id} column={column} />
-      <div
+      <motion.div
+        layout
+        layoutId={id}
         draggable
         onDragStart={(event) => hanldeDragStart(event, card)}
         className="cursor-grab rounded  border border-neutral-700 bg-neutral-800 p-3 active:cursor-grabbing">
         <p className="text-sm text-neutral-100">{title}</p>
-      </div>
+      </motion.div>
     </>
   );
 };
