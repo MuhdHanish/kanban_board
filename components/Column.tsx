d691857cef95d5a3d7c26529f1b4afd6546797a8
MuhdHanish <muhammedhanish11@gmail.com>
2024-05-05 09:01:13 +0530

    added cards and drop indicator in column

--- components/Column.tsx
+++ components/Column.tsx
@@ -1,7 +1,24 @@
+import { Card } from "./Card";
+import { useState } from "react";
 import { TColumnProps } from "@/types";
+import { DropIndicator } from "./DropIndicator";
 
-export const Column = ({ }: TColumnProps) => {
+export const Column = ({ title, headingColor, cards, column }: TColumnProps) => {
+  const [active, setActive] = useState(false);
+  const filteredCards = cards?.filter(card => card?.column === column);
   return (
-    <></>
-  )
-}
+    <div className="w-56 shrink-0">
+      <div className="mb-3 flex items-center justify-between">
+        <h3 className={`font-medium ${headingColor}`}>{title}</h3>
+        <span className="text-sm text-neutral-400">{filteredCards?.length}</span>
+      </div>
+      <div
+        className={`h-full w-full transition-colors 
+        ${active ? "bg-neutral-800/50" : "bg-neutral-800/0"}`}
+      >
+        {filteredCards?.map(card => <Card key={card?.id} {...card} />)}
+        <DropIndicator beforeId="-1" column={column} />
+      </div>
+    </div>
+  );
+};
