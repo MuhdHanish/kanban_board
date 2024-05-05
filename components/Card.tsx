bd77e3374a4815aebeb12bef005d317c2dddf868
MuhdHanish <muhammedhanish11@gmail.com>
2024-05-05 10:33:25 +0530

    added handelDragStart function in onDragStart

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,12 +1,14 @@
-import { TCard } from "@/types";
+import { TCardProps } from "@/types";
 import { DropIndicator } from "./DropIndicator";
 
-export const Card = ({ id, title, column }: TCard) => {
+export const Card = ({ card, hanldeDragStart }: TCardProps) => {
+  const { id, title, column } = card;
   return (
     <>
       <DropIndicator beforeId={id} column={column} />
       <div
         draggable
+        onDragStart={(event) => hanldeDragStart(event, card)}
         className="cursor-grab rounded  border border-neutral-700 bg-neutral-800 p-3 active:cursor-grabbing">
         <p className="text-sm text-neutral-100">{title}</p>
       </div>
