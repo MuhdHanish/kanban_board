a24896e6e61469459bfb885b05beb2de8243ef52
MuhdHanish <muhammedhanish11@gmail.com>
2024-05-06 08:26:53 +0530

    updated package.json

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,13 @@
+import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Kanban Board",
   description: "A dynamic and interactive Kanban board built with Next.js, Tailwind CSS, and Framer Motion.",
-  keywords: ["Kanban", "Board", "Next.js", "Tailwind CSS", "Framer Motion", "Project Management", "Workflow Management"],
+  keywords: ["Kanban", "Board", "Next.js", "Typescript", "Tailwind CSS", "Framer Motion", "Project Management", "Workflow Management"],
 };
 
 export default function RootLayout({
